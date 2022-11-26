1d3296078e1d74f1d865979cf3ec94191affcf28
Julio Moura <juliomourajr@hotmail.com>
2022-11-26 03:31:21 -0300

    Melhorando a logica dos arquivos.

--- tipos-e-variaveis/src/TiposEVariaveis.java
+++ tipos-e-variaveis/src/TiposEVariaveis.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
 
 public class TiposEVariaveis {
     public static void main(String[] args) {
-        String nome = args[0];
-        String sobrenome = args[1];
-        final int idade = Integer.valueOf(args[2]);
+        String nome = "";
+        String sobrenome = "";
+        int idade = 0;
 
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Qual é o seu nome? ");
+        nome = leitor.next();
+
+        System.out.println("Qual é o seu sobrenome? ");
+        sobrenome = leitor.next();
+
+        System.out.println("Qual é a sua idade? ");
+        idade = leitor.nextInt();
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Tenho " + idade + " anos.");
 
     }
 }
